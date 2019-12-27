92125e86a68f567f0130409258f4ca1c936d6cd6
flooji <florella240@gmail.com>
2019-12-27 15:17:38 +0100

    upload correct claim

--- authenticate/tokenGenerator.js
+++ authenticate/tokenGenerator.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 //sign claim -> generate JSON web token
 exports.token = () => {
-    const payload =  fs.readFileSync('./authenticate/claim_9f412cc5-ee81-4e54-b493-1a5528b5f367.json')
+    const payload =  fs.readFileSync('./authenticate/IDclaim.json')
     const privateKey = fs.readFileSync('./authenticate/privateKey.pem')
     return jwt.sign(payload, privateKey,{ algorithm: 'RS256' })
 }
