e254696e3bf4c7f071de26eb1deda6bb927409b7
florence <32374594+flooji@users.noreply.github.com>
2020-01-04 15:16:09 +0100

    clean up api.js

--- api.js
+++ api.js
@@ -7,8 +7,8 @@ var fs = require('fs')
 var app = express()
 app.use(express['static'](__dirname ))
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
 
