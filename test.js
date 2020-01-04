3688304cdc6adef12c36b169dacf202b50324db7
florence <32374594+flooji@users.noreply.github.com>
2020-01-04 15:21:16 +0100

    delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-let {token} = require('./authenticate/tokenGenerator')
-let fs = require('fs')
-
-const claim =  fs.readFileSync('./authenticate/IDclaim.json')
-const privateKey = fs.readFileSync('./authenticate/privateKey.pem')
-
-token(claim,privateKey)
